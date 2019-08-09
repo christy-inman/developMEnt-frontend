ef699819d6160d1e9832a7469320deb4250b5300
github username <github email address>
2019-08-09 11:27:15 -0600

    title bar

---
+++ src/components/TitleBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function titleBar() {
+    return(
+        <div className="title-bar">
+            <h1>developMEnt</h1>
+            <h2>Login:</h2>
+            <input type="text" />
+        </div>
+    )
+}
